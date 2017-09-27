3f9def6b375974c9011d6411eff27fec3535ad80
Nikhil Lohia <nikoo28@gmail.com>
2017-09-27 05:43:48 -0700

    Project Setup

---
+++ Assignments/HealthMonitor/app/src/main/java/com/nikoo28/healthmonitor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nikoo28.healthmonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
